71f56436db1d3425b501bec262e965e92e68dd30
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2025-08-27 19:07:30 +0300

    Add authentication routes and JWT token generation

---
+++ back/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+const bcrypt = require("bcrypt");
+const generateToken = require("../utils/jwtGenerator.js");
+const User = require("../models/user.model.js");
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.find({
+      username,
+    });
+    if (user.length > 0) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.find({
+      username,
+    });
+    if (user.length === 0) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user[0].password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const token = generateToken(user[0]);
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user[0]._id,
+        username: user[0].username,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
--- back/server.js
+++ back/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const DB = require("./config/db.js");
+const morgan = require("morgan");
 cors.options = {
   origin: "http://localhost:5173",
 };
 
 const app = express();
-
+app.use(morgan("dev"));
 app.use(cors(cors.options));
 app.use(express.json());
-
+app.use("/api/auth", require("./routes/auth.route.js"));
 DB.connectDB();
 
 app.listen(3000, () => {
