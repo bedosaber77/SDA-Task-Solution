602ec838f7a78cc29e891c8121713d72887ba088
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2025-08-29 09:14:13 +0300

    feat: add authentication context and protected route component

---
+++ front/src/components/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import {type ReactNode} from "react";
+
+export default function ProtectedRoute({ children: children }: { children: ReactNode}) {
+  const { token } = useAuth();
+  if (!token) {
+    console.log("No token found, redirecting to login.");
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+}
---
+++ front/src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useContext, type ReactNode } from "react";
+
+export interface AuthContextType {
+  token: string | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token") ?? null
+  );
+
+  const login = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem("token", newToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  const providerValue = { token, login, logout };
+
+  return (
+    <AuthContext.Provider value={providerValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used inside an AuthProvider");
+  }
+  return context;
+};
